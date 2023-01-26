ae1d71b1b403c58272b3d047ce37d634e2279fd5
John Munson <johncmunson91@gmail.com>
2023-01-26 08:26:57 -0600

    update

--- index.test.ts
+++ index.test.ts
@@ -1,10 +1,27 @@
-import { compact, chunk } from './index'
+import {
+  compact,
+  chunk,
+  getAllKeys,
+  sumValues,
+  arrayDifference,
+  addOrRemove,
+} from './index'
 
 describe('compact', () => {
   it('takes an array with falsy values and returns a new array with the falsy values removed', () => {
     const before = ['a', false, 0, '', null, undefined, NaN, 'b']
     const after = ['a', 'b']
     expect(compact(before)).toEqual(after)
+  })
+})
+
+describe('addOrRemove', () => {
+  it("adds an item to an array if it doesn't already exist", () => {
+    expect(addOrRemove(5, ['a', true, 3])).toEqual(['a', true, 3, 5])
+  })
+
+  it('removes an item from an array if it already exists', () => {
+    expect(addOrRemove(7, ['a', true, 7])).toEqual(['a', true])
   })
 })
 
@@ -38,5 +55,41 @@ describe('chunk', () => {
     const after = [[1, 2, 3]]
     expect(chunk(before, size1)).toEqual(after)
     expect(chunk(before, size2)).toEqual(after)
+  })
+})
+
+describe('sumValues', () => {
+  it('sums the numbers in an array', () => {
+    expect(sumValues([1, 2, 3])).toEqual(6)
+    expect(sumValues([0, -1, 2, -3])).toEqual(-2)
+    expect(sumValues([])).toEqual(0)
+  })
+})
+
+describe('arrayDifference', () => {
+  it('calculates the difference between the first array and all the others', () => {
+    expect(arrayDifference([1, 2, 3, 4, 5], [2, 3, 9], [5, 7])).toEqual([1, 4])
+  })
+})
+
+describe('getAllKeys', () => {
+  it('returns all the keys of a nested object', () => {
+    const nestedObj = {
+      abc: 123,
+      xyz: true,
+      foo: {
+        bar: {
+          baz: [4, 5, 6],
+        },
+      },
+    }
+    const expectedKeys = ['abc', 'xyz', 'foo', 'bar', 'baz', '0', '1', '2']
+    expect(getAllKeys(nestedObj)).toEqual(expectedKeys)
+  })
+
+  it('returns all the keys of a flat object', () => {
+    const flatObj = { a: 1, b: 2, c: 3 }
+    const expectedKeys = ['a', 'b', 'c']
+    expect(getAllKeys(flatObj)).toEqual(expectedKeys)
   })
 })
--- index.ts
+++ index.ts
@@ -1,6 +1,13 @@
 // Write a function called `compact` that removes all falsy values from an array
 export const compact = <T>(arr: T[]) => arr.filter((val) => val)
 
+// Write a function called `addOrRemove` that adds an item to an array if it doesn't already exist, and removes it if not.
+// The array can be heterogeneous, but only contain primitive values.
+export const addOrRemove = <T extends Array<number | string | boolean>>(
+  item: T[number],
+  arr: T
+) => (arr.includes(item) ? arr.filter((val) => val != item) : [...arr, item])
+
 // Write a function called `chunk` that accepts an array and splits it into groups of length `size`. If the array can't be split evenly, the final chunk will be the remaining elements.
 export const chunk = <T>(arr: T[], size: number) => {
   const chunkedArr: T[][] = []
@@ -11,9 +18,23 @@ export const chunk = <T>(arr: T[], size: number) => {
   return chunkedArr
 }
 
-// Write a program to calculate the sum of all the values of an array
+// Write function called `sumValues` to calculate the sum of all the number in an array
+export const sumValues = (numbers: number[]) =>
+  numbers.reduce((prev, current) => prev + current, 0)
 
-// Get the list of all distinct elements which are present in both list of numbers
+// Write a function called `arrayDifference` that returns all the values from the first array that are not present in any of the other provided arrays.
+export const arrayDifference = <T>(arr: T[], ...others: T[][]) =>
+  arr.filter((val) => !others.some((otherArr) => otherArr.includes(val)))
 
-// Return a flattened array of all the keys from a nested object
-// https://github.com/sadanandpai/javascript-code-challenges/blob/main/challenges/objects-challenges.md#q-display-all-the-keys-and-values-of-a-nested-object
+// Write a function called `getAllKeys` that returns all the keys of a nested object
+export const getAllKeys = (obj: Object, keys: string[] = []) => {
+  for (let key in obj) {
+    keys.push(key)
+    if (typeof obj[key] === 'object') {
+      getAllKeys(obj[key], keys)
+    }
+  }
+  return keys
+}
+
+// class, sleep, memoize
